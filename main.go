4ae1e4fd3c59344c7f4ba8838cfc1cc8bf96f432
luiscdo <luiscdo1997@gmail.com>
2018-04-11 18:07:52 -0400

    inicio 0.1

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+	cad := "arepera"
+	fmt.Println(cad)
+	fmt.Println("******")
+	fmt.Println(len(cad))
+	fmt.Println("******")
+	fmt.Println(cad[0:5])
+	fmt.Println("******")
+	fmt.Println("Ingrese un numero")
+	var name int
+	fmt.Scanln(&name)
+	fmt.Println("******")
+	fmt.Println(name)
+}
